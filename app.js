b3f2f6d5a2272fea280a1a90484b3195fcba16b7
zyrs <it_tangzhang@163.com>
2019-03-19 09:17:15 +0800

    结构整改

---
+++ app.js
@@ -0,0 +1,12 @@
+//app.js
+App({
+  onLaunch: function(){
+  
+  },
+  globalData: {
+    userInfo:null,
+    host:"https://api.zyrs.xyz:10000/wx_tz/",
+    imgurl:"https://api.zyrs.xyz:10000/wx_tz/img",
+    imgageUploadUrl:        'https://api.zyrs.xyz:10000/wx_tz/img/component/picture-cut.png',
+  }
+})
